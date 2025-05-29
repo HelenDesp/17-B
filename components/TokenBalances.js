e6ddd842f6f061250ed14b240ddfc3b1003f3721
HelenDesp <helenadespit@gmail.com>
2025-05-30 00:43:23 +0300

    Add files via upload

--- components/TokenBalances.js
+++ components/TokenBalances.js
@@ -184,7 +184,7 @@ export default function TokenBalances() {
           </div>
           <div className="text-right">
             <div className="font-medium text-gray-900 dark:text-white">
-              {ethBalance ? Number(ethBalance.formatted).toFixed(4) : "0.0000"}{" "}
+              {ethBalance ? Number(ethBalance.formatted).toFixed(5) : "0.00000"}{" "}
               ETH
             </div>
             <div className="text-xs text-gray-500 dark:text-gray-400 hidden sm:block">
