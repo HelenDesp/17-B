8964f58907c9759ff58d8abd7585d223b9f922f6
HelenDesp <helenadespit@gmail.com>
2025-05-30 05:06:27 +0300

    Add files via upload

--- components/NFTViewer.js
+++ components/NFTViewer.js
@@ -106,6 +106,7 @@ export default function NFTViewer() {
                   onChange={e => handleChange("name", e.target.value)}
                   placeholder={selectedNFT.name}
                   className="w-full p-2 !border !border-black dark:!border-white !bg-gray-50 dark:!bg-gray-700 !text-gray-900 dark:!text-white placeholder-gray-400 focus:placeholder-transparent focus:!border-2 !rounded-none focus:outline-none focus:ring-0"
+				  style={{ boxShadow: 'none', borderRadius: '0px' }}
                 />
               </div>
               {["manifesto", "friend", "weapon"].map(field => (
@@ -116,6 +117,7 @@ export default function NFTViewer() {
                     onChange={e => handleChange(field, e.target.value)}
                     placeholder={selectedNFT.traits[field]}
                     className="w-full p-2 !border !border-black dark:!border-white !bg-gray-50 dark:!bg-gray-700 !text-gray-900 dark:!text-white placeholder-gray-400 focus:placeholder-transparent focus:!border-2 !rounded-none focus:outline-none focus:ring-0"
+					style={{ boxShadow: 'none', borderRadius: '0px' }}
                   />
                 </div>
               ))}
@@ -144,12 +146,12 @@ export default function NFTViewer() {
 			<div className="relative bg-white dark:bg-gray-800 p-10 rounded shadow-lg max-w-lg w-full text-center">
 			  
 			  {/* Close Icon */}
-			  <button
-				className="absolute top-3 right-3 text-lg text-gray-800 dark:text-white border-2 border-black dark:border-white w-8 h-8 flex items-center justify-center hover:bg-gray-100 dark:hover:bg-gray-700 transition"
-				onClick={() => setShowThankYou(false)}
-			  >
-				&times;
-			  </button>
+				<button
+				  className="absolute top-4 right-4 w-10 h-10 border-2 border-black dark:border-white text-2xl font-bold text-gray-800 dark:text-white hover:bg-gray-900 hover:text-white dark:hover:bg-white dark:hover:text-black transition-all [font-family:'Cygnito_Mono',sans-serif] leading-none"
+				  onClick={() => setShowThankYou(false)}
+				>
+				  ×
+				</button>
 
 			  {/* Message */}
 			  <h4 className="text-2xl font-bold text-gray-900 dark:text-white mb-4">
