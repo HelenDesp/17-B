821b9c9f253cbef8d2893cbe819b7b1732c1d850
HelenDesp <helenadespit@gmail.com>
2025-06-05 19:47:31 +0300

    Add files via upload

--- components/NFTViewer.js
+++ components/NFTViewer.js
@@ -75,11 +75,11 @@ export default function NFTViewer({
   <div className="relative flex flex-col items-center">
     <div
       className="opacity-0 group-hover:opacity-100 transition pointer-events-none absolute left-1/2 -translate-x-[88%] bottom-[14px] z-50"
-      style={{ width: 24, height: 24 }}
+      style={{ width: 22, height: 22 }}
     >
       {/* Auto-dark/light plane icon */}
       <svg version="1.0" xmlns="http://www.w3.org/2000/svg"
-        width="24" height="24" viewBox="0 0 512 512"
+        width="22" height="22" viewBox="0 0 512 512"
         className="w-7 h-7 fill-black dark:fill-white"
         preserveAspectRatio="xMidYMid meet"
       >
