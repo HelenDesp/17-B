643ad37feb7db60c624196eedee81a035bf6b04c
HelenDesp <helenadespit@gmail.com>
2025-06-16 19:56:09 +0300

    Add files via upload

--- components/TokenTxHistory.js
+++ components/TokenTxHistory.js
@@ -34,6 +34,19 @@ const getChainLabel = (chainId) => {
   }
 };  
 
+const getExplorerBaseUrl = (chainId) => {
+  switch (chainId) {
+    case 1: return "https://etherscan.io";
+    case 8453: return "https://basescan.org";
+    case 137: return "https://polygonscan.com";
+    case 42161: return "https://arbiscan.io";
+    case 10: return "https://optimistic.etherscan.io";
+    case 56: return "https://bscscan.com";
+    case 11155111: return "https://sepolia.etherscan.io";
+    default: return "https://etherscan.io";
+  }
+};
+
 useEffect(() => {
   if (!address || !chainId) return;
 
@@ -185,54 +198,56 @@ useEffect(() => {
                   {expandedIndexes[i] ? "Hide" : "Details"}
                 </button>
               </div>
-              <div className="flex justify-between text-sm mt-1">
-                <div className="text-black dark:text-white">{tx.asset || "ETH"} – {tx.value}</div>
-                <div className="text-black dark:text-white">{formatShortDate(tx.metadata.blockTimestamp)}</div>
-              </div>
-<div className="flex items-center justify-between text-black dark:text-white">
-  <a
-    href={`https://basescan.org/tx/${tx.hash}`}
-    target="_blank"
-    rel="noopener noreferrer"
-    className="underline"
-  >
-    View on Explorer
-  </a>
+				<div className="flex justify-between text-sm mt-1">
+				  <div className="text-black dark:text-white">
+					{tx.asset || "ETH"} – {parseFloat(tx.value).toFixed(5)}
+				  </div>
+				  <div className="text-black dark:text-white">{formatShortDate(tx.metadata.blockTimestamp)}</div>
+				</div>
+				<div className="flex items-center justify-between text-black dark:text-white">
+				  <a
+					href={`${getExplorerBaseUrl(chainId)}/tx/${tx.hash}`}
+					target="_blank"
+					rel="noopener noreferrer"
+					className="underline"
+				  >
+					View on Explorer
+				  </a>
 
-  {tx._type?.startsWith("Sent") && (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 256 256"
-      className="w-5 h-5 ml-2 fill-black dark:fill-white"
-    >
-      <path d="M77.17187,170.82861a3.99971,3.99971,0,0,1,0-5.65722L182.34277,60H92a4,4,0,0,1,0-8H192c.0166,0,.03174.0047.04834.00488a4.02377,4.02377,0,0,1,.73437.074c.12159.02411.23389.06927.35108.10412a2.30534,2.30534,0,0,1,.77588.32282c.103.05633.21045.10168.30908.16772a4.02182,4.02182,0,0,1,.58691.47919c.00684.007.01563.01154.02246.01862l.01417.01684a4.0149,4.0149,0,0,1,.48388.5929c.06738.1001.11328.20856.16992.313a3.85529,3.85529,0,0,1,.19776.37573,3.97336,3.97336,0,0,1,.12646.40607c.03321.114.07715.223.10059.34094A3.98826,3.98826,0,0,1,196,56V156a4,4,0,0,1-8,0V65.657L82.82812,170.82861a3.99971,3.99971,0,0,1-5.65625,0ZM216,211.99609H40a4,4,0,0,0,0,8H216a4,4,0,0,0,0-8Z" />
-    </svg>
-  )}
+				  {tx._type?.startsWith("Sent") && (
+					<svg
+					  xmlns="http://www.w3.org/2000/svg"
+					  viewBox="0 0 256 256"
+					  className="w-5 h-5 ml-2 fill-black dark:fill-white"
+					>
+					  <path d="M77.17187,170.82861a3.99971,3.99971,0,0,1,0-5.65722L182.34277,60H92a4,4,0,0,1,0-8H192c.0166,0,.03174.0047.04834.00488a4.02377,4.02377,0,0,1,.73437.074c.12159.02411.23389.06927.35108.10412a2.30534,2.30534,0,0,1,.77588.32282c.103.05633.21045.10168.30908.16772a4.02182,4.02182,0,0,1,.58691.47919c.00684.007.01563.01154.02246.01862l.01417.01684a4.0149,4.0149,0,0,1,.48388.5929c.06738.1001.11328.20856.16992.313a3.85529,3.85529,0,0,1,.19776.37573,3.97336,3.97336,0,0,1,.12646.40607c.03321.114.07715.223.10059.34094A3.98826,3.98826,0,0,1,196,56V156a4,4,0,0,1-8,0V65.657L82.82812,170.82861a3.99971,3.99971,0,0,1-5.65625,0ZM216,211.99609H40a4,4,0,0,0,0,8H216a4,4,0,0,0,0-8Z" />
+					</svg>
+				  )}
 
-  {tx._type?.startsWith("Received") && (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 256 256"
-      className="w-5 h-5 ml-2 fill-black dark:fill-white"
-    >
-      <path d="M53.17187,114.82471a3.99992,3.99992,0,0,1,5.65625-5.65723L124,174.33936V31.99609a4,4,0,0,1,8,0V174.33936l65.17187-65.17188a3.99992,3.99992,0,1,1,5.65625,5.65723l-72,72c-.00683.00708-.01562.01172-.02246.01855a4.01055,4.01055,0,0,1-.58691.47925c-.10059.06714-.209.11328-.314.17041a3.961,3.961,0,0,1-.37452.197,3.91774,3.91774,0,0,1-.40918.12695c-.11279.03321-.2207.07715-.33789.1001a3.91693,3.91693,0,0,1-1.5664,0c-.11719-.023-.2251-.06689-.33789-.1001a3.91774,3.91774,0,0,1-.40918-.12695,3.961,3.961,0,0,1-.37452-.197c-.105-.05713-.21337-.10327-.314-.17041a4.01055,4.01055,0,0,1-.58691-.47925c-.00684-.00683-.01563-.01147-.02247-.01855ZM216,211.99609H40a4,4,0,0,0,0,8H216a4,4,0,0,0,0-8Z" />
-    </svg>
-  )}
+				  {tx._type?.startsWith("Received") && (
+					<svg
+					  xmlns="http://www.w3.org/2000/svg"
+					  viewBox="0 0 256 256"
+					  className="w-5 h-5 ml-2 fill-black dark:fill-white"
+					>
+					  <path d="M53.17187,114.82471a3.99992,3.99992,0,0,1,5.65625-5.65723L124,174.33936V31.99609a4,4,0,0,1,8,0V174.33936l65.17187-65.17188a3.99992,3.99992,0,1,1,5.65625,5.65723l-72,72c-.00683.00708-.01562.01172-.02246.01855a4.01055,4.01055,0,0,1-.58691.47925c-.10059.06714-.209.11328-.314.17041a3.961,3.961,0,0,1-.37452.197,3.91774,3.91774,0,0,1-.40918.12695c-.11279.03321-.2207.07715-.33789.1001a3.91693,3.91693,0,0,1-1.5664,0c-.11719-.023-.2251-.06689-.33789-.1001a3.91774,3.91774,0,0,1-.40918-.12695,3.961,3.961,0,0,1-.37452-.197c-.105-.05713-.21337-.10327-.314-.17041a4.01055,4.01055,0,0,1-.58691-.47925c-.00684-.00683-.01563-.01147-.02247-.01855ZM216,211.99609H40a4,4,0,0,0,0,8H216a4,4,0,0,0,0-8Z" />
+					</svg>
+				  )}
 
-  {tx._type?.startsWith("Swapped") && (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 256 256"
-      className="w-5 h-5 ml-2 fill-black dark:fill-white"
-    >
-      <path d="M132,40V216a4,4,0,0,1-8,0V40a4,4,0,0,1,8,0ZM99.69434,129.52966a3.95348,3.95348,0,0,0,.12548-.40527c.0337-.11389.07764-.223.10108-.34094a4.01026,4.01026,0,0,0,0-1.5669c-.02344-.11792-.06738-.22693-.10108-.34082a3.95868,3.95868,0,0,0-.12548-.40552,4.0404,4.0404,0,0,0-.20362-.38623c-.05517-.10058-.09912-.20532-.16357-.30175a4.02777,4.02777,0,0,0-.50147-.61292L66.82812,93.17188a3.99957,3.99957,0,0,0-5.65624,5.65624L86.34277,124H16a4,4,0,0,0,0,8H86.34277L61.17188,157.17188a3.99957,3.99957,0,1,0,5.65624,5.65624l31.99756-31.99743a4.02777,4.02777,0,0,0,.50147-.61292c.06445-.09643.1084-.20129.16357-.302A4.00758,4.00758,0,0,0,99.69434,129.52966ZM240,124H169.65723l25.17089-25.17188a3.99957,3.99957,0,1,0-5.65624-5.65624l-31.99756,31.99743a4.02777,4.02777,0,0,0-.50147.61292c-.06445.097-.10889.20239-.16455.30359a2.34888,2.34888,0,0,0-.32861.79162c-.03369.1134-.07715.2218-.10059.33911a4.01026,4.01026,0,0,0,0,1.5669c.02344.11731.06738.22583.10059.33911a2.34963,2.34963,0,0,0,.32861.7915c.05566.1012.1001.20655.16455.30371a4.02777,4.02777,0,0,0,.50147.61292l31.99756,31.99743a3.99957,3.99957,0,0,0,5.65624-5.65624L169.65723,132H240a4,4,0,0,0,0-8Z" />
-    </svg>
-  )}
-</div>
+				  {tx._type?.startsWith("Swapped") && (
+					<svg
+					  xmlns="http://www.w3.org/2000/svg"
+					  viewBox="0 0 256 256"
+					  className="w-5 h-5 ml-2 fill-black dark:fill-white"
+					>
+					  <path d="M132,40V216a4,4,0,0,1-8,0V40a4,4,0,0,1,8,0ZM99.69434,129.52966a3.95348,3.95348,0,0,0,.12548-.40527c.0337-.11389.07764-.223.10108-.34094a4.01026,4.01026,0,0,0,0-1.5669c-.02344-.11792-.06738-.22693-.10108-.34082a3.95868,3.95868,0,0,0-.12548-.40552,4.0404,4.0404,0,0,0-.20362-.38623c-.05517-.10058-.09912-.20532-.16357-.30175a4.02777,4.02777,0,0,0-.50147-.61292L66.82812,93.17188a3.99957,3.99957,0,0,0-5.65624,5.65624L86.34277,124H16a4,4,0,0,0,0,8H86.34277L61.17188,157.17188a3.99957,3.99957,0,1,0,5.65624,5.65624l31.99756-31.99743a4.02777,4.02777,0,0,0,.50147-.61292c.06445-.09643.1084-.20129.16357-.302A4.00758,4.00758,0,0,0,99.69434,129.52966ZM240,124H169.65723l25.17089-25.17188a3.99957,3.99957,0,1,0-5.65624-5.65624l-31.99756,31.99743a4.02777,4.02777,0,0,0-.50147.61292c-.06445.097-.10889.20239-.16455.30359a2.34888,2.34888,0,0,0-.32861.79162c-.03369.1134-.07715.2218-.10059.33911a4.01026,4.01026,0,0,0,0,1.5669c.02344.11731.06738.22583.10059.33911a2.34963,2.34963,0,0,0,.32861.7915c.05566.1012.1001.20655.16455.30371a4.02777,4.02777,0,0,0,.50147.61292l31.99756,31.99743a3.99957,3.99957,0,0,0,5.65624-5.65624L169.65723,132H240a4,4,0,0,0,0-8Z" />
+					</svg>
+				  )}
+				</div>
               {expandedIndexes[i] && (
                 <div className="mt-2 space-y-1 text-xs">
-                  <div className="text-black dark:text-white"><strong>From:</strong> <a href={`https://basescan.org/address/${tx.from}`} target="_blank" rel="noopener noreferrer" className="underline text-black dark:text-white">{shortenAddress(tx.from)}</a></div>
-                  <div className="text-black dark:text-white"><strong>To:</strong> <a href={`https://basescan.org/address/${tx.to}`} target="_blank" rel="noopener noreferrer" className="underline text-black dark:text-white">{shortenAddress(tx.to)}</a></div>
+                  <div className="text-black dark:text-white"><strong>From:</strong> <a href={`${getExplorerBaseUrl(chainId)}/address/${tx.from}`} target="_blank" rel="noopener noreferrer" className="underline text-black dark:text-white">{shortenAddress(tx.from)}</a></div>
+                  <div className="text-black dark:text-white"><strong>To:</strong> <a href={`${getExplorerBaseUrl(chainId)}/address/${tx.to}`} target="_blank" rel="noopener noreferrer" className="underline text-black dark:text-white">{shortenAddress(tx.to)}</a></div>
 					<div className="text-black dark:text-white">
 					  <strong>Block:</strong> {parseInt(tx.blockNum, 16)}
 					</div>
