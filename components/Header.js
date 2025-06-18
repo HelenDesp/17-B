957b3e6194164cd5ae334af48b892cf09ce2cfe0
HelenDesp <helenadespit@gmail.com>
2025-06-18 11:05:19 +0300

    Add files via upload

--- components/Header.js
+++ components/Header.js
@@ -231,7 +231,7 @@ useEffect(() => {
 ) : (
   <button
     onClick={() => open({ view: "Connect" })}
-    className="mx-auto block border-4 border-gray-900 dark:border-white bg-light-100 text-gray-900 dark:bg-dark-300 dark:text-white px-6 py-2 text-sm [font-family:'Cygnito_Mono',sans-serif] uppercase tracking-wide rounded-none transition-colors duration-200 hover:bg-gray-900 hover:text-white dark:hover:bg-white dark:hover:text-black"
+    className="xsm:px-2 px-4 py-1.5 border-2 border-gray-900 dark:border-white bg-light-100 text-gray-900 dark:bg-dark-300 dark:text-white text-sm [font-family:'Cygnito_Mono',sans-serif] uppercase tracking-wide rounded-none transition-colors duration-200 hover:bg-gray-900 hover:text-white dark:hover:bg-white dark:hover:text-black"
   >
     Connect Wallet
   </button>
