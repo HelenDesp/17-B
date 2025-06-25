8b6d1eafbef79e70039356f907f171dcdedff096
HelenDesp <helenadespit@gmail.com>
2025-06-25 15:01:49 +0300

    Add files via upload

--- components/NftTxHistory.js
+++ components/NftTxHistory.js
@@ -217,66 +217,58 @@ export default function NftTxHistory({ address, chainId }) {
 	const [expandedIndexes, setExpandedIndexes] = useState({});
     const paginated = txs.slice((page - 1) * perPage, page * perPage);
 
-  return (
+return (
     <div className="p-4 bg-white border-b2 dark:bg-dark-200 shadow">
-	<div className="flex justify-between items-center mb-2">
-	  <h3 className="text-lg font-semibold text-gray-900 dark:text-white">NFT Transactions</h3>
-	  <span className="text-sm text-gray-500 dark:text-gray-400 uppercase">{getChainLabel(chain?.id)}</span>
-	</div>
+      <div className="flex justify-between items-center mb-2">
+        <h3 className="text-lg font-semibold text-gray-900 dark:text-white">NFT Transactions</h3>
+        <span className="text-sm text-gray-500 dark:text-gray-400 uppercase">{getChainLabel(chain?.id)}</span>
+      </div>
       <div className="space-y-2">
         {paginated.length === 0 ? (
           <p className="text-gray-600 dark:text-white text-sm">No recent NFT transactions.</p>
         ) : (
           paginated.map((tx, i) => {
-            // MODIFIED: Use a stable, unique key to store and retrieve the name
             const uniqueKey = `${chainId}-${tx.rawContract.address}-${tx.tokenId}`;
             const displayName = nftNames[uniqueKey] || tx.asset;
             
             return (
               <div key={`${tx.hash}-${i}`} className="text-sm text-gray-200 dark:text-gray-100 border-b border-gray-200 dark:border-gray-100 pb-2">
-              <div className="flex items-center gap-3 w-full">
-
-                <div className="flex-shrink-0">
-                  <NftImage 
-                    contractAddress={tx.rawContract.address} 
-                    tokenId={tx.tokenId} 
-                    cache={metadataCache}
-                    chainId={chainId}
-                    onNameLoaded={(name) => {
-                      setNftNames(prev => ({ ...prev, [uniqueKey]: name }));
-                    }}
-                  />
-                </div>
-
-                <div className="flex-grow min-w-0">
-
-                  <div className="flex justify-between items-center">
-                    <div className="text-black dark:text-white"><strong>{tx._type}</strong></div>
-                    <div className="flex items-center">
-                      {tx._type === "Sent" && <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" className="w-5 h-5 ml-2 fill-black dark:fill-white"><path d="M77.17187,170.82861a3.99971,3.99971,0,0,1,0-5.65722L182.34277,60H92a4,4,0,0,1,0-8H192c.0166,0,.03174.0047.04834.00488a4.02377,4.02377,0,0,1,.73437.074c.12159.02411.23389.06927.35108.10412a2.30534,2.30534,0,0,1,.77588.32282c.103.05633.21045.10168.30908.16772a4.02182,4.02182,0,0,1,.58691.47919c.00684.007.01563.01154.02246.01862l.01417.01684a4.0149,4.0149,0,0,1,.48388.5929c.06738.1001.11328.20856.16992.313a3.85529,3.85529,0,0,1,.19776.37573,3.97336,3.97336,0,0,1,.12646.40607c.03321.114.07715.223.10059.34094A3.98826,3.98826,0,0,1,196,56V156a4,4,0,0,1-8,0V65.657L82.82812,170.82861a3.99971,3.99971,0,0,1-5.65625,0ZM216,211.99609H40a4,4,0,0,0,0,8H216a4,4,0,0,0,0-8Z" /></svg>}
-                      {tx._type === "Received" && <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" className="w-5 h-5 ml-2 fill-black dark:fill-white"><path d="M53.17187,114.82471a3.99992,3.99992,0,0,1,5.65625-5.65723L124,174.33936V31.99609a4,4,0,0,1,8,0V174.33936l65.17187-65.17188a3.99992,3.99992,0,1,1,5.65625,5.65723l-72,72c-.00683.00708-.01562.01172-.02246.01855a4.01055,4.01055,0,0,1-.58691.47925c-.10059.06714-.209.11328-.314.17041a3.961,3.961,0,0,1-.37452.197,3.91774,3.91774,0,0,1-.40918.12695c-.11279.03321-.2207.07715-.33789.1001a3.91693,3.91693,0,0,1-1.5664,0c-.11719-.023-.2251-.06689-.33789-.1001a3.91774,3.91774,0,0,1-.40918-.12695,3.961,3.961,0,0,1-.37452-.197c-.105-.05713-.21337-.10327-.314-.17041a4.01055,4.01055,0,0,1-.58691-.47925c-.00684-.00683-.01563-.01147-.02247-.01855ZM216,211.99609H40a4,4,0,0,0,0,8H216a4,4,0,0,0,0-8Z" /></svg>}
-                      {tx._type === "Burned" && <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" className="w-5 h-5 ml-2 fill-black dark:fill-white"><path d="M108,40a4.0002,4.0002,0,0,1,4-4h32a4,4,0,0,1,0,8H112A4.0002,4.0002,0,0,1,108,40Zm36,172H112a4,4,0,0,0,0,8h32a4,4,0,0,0,0-8ZM208,36H184a4,4,0,0,0,0,8h24a4.00458,4.00458,0,0,1,4,4V71.99951a4,4,0,0,0,8,0V48A12.01375,12.01375,0,0,0,208,36Zm8,71.99951a4.0002,4.0002,0,0,0-4,4v32a4,4,0,0,0,8,0v-32A4.0002,4.0002,0,0,0,216,107.99951Zm-176,40a4.0002,4.0002,0,0,0,4-4v-32a4,4,0,1,0-8,0v32A4.0002,4.0002,0,0,0,40,147.99951ZM72,212H48a4.00427,4.00427,0,0,1-4-4V184a4,4,0,0,0-8,0v24a12.01343,12.01343,0,0,0,12,12H72a4,4,0,0,0,0-8ZM50.96,37.30908A4.0003,4.0003,0,0,0,45.04,42.69092l160,175.99951a4,4,0,1,0,5.91992-5.38086Z" /></svg>}
-                      {tx._type === "Minted" && <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" className="w-5 h-5 ml-2 fill-black dark:fill-white"><path d="M208,36H48A12.01312,12.01312,0,0,0,36,48V208a12.01312,12.01312,0,0,0,12,12H208a12.01312,12.01312,0,0,0,12-12V48A12.01312,12.01312,0,0,0,208,36Zm4,172a4.004,4.004,0,0,1-4,4H48a4.004,4.004,0,0,1-4-4V177.65631l33.17187-33.171a4.00208,4.00208,0,0,1,5.65723,0l20.68652,20.68652a12.011,12.011,0,0,0,16.96973,0l44.68652-44.68652a4.00208,4.00208,0,0,1,5.65723,0L212,161.65625Zm0-57.65625L176.48535,114.8291a11.99916,11.99916,0,0,0-16.96973,0L114.8291,159.51562a4.00681,4.00681,0,0,1-5.65723,0L88.48535,138.8291a12.01009,12.01009,0,0,0-16.96973,0L44,166.34393V48a4.004,4.004,0,0,1,4-4H208a4.004,4.004,0,0,1,4,4ZM108.001,92v.00195a8.001,8.001,0,1,1,0-.00195Z" /></svg>}
+                <div className="flex items-center gap-3 w-full">
+                  <div className="flex-shrink-0">
+                    <NftImage 
+                      contractAddress={tx.rawContract.address} 
+                      tokenId={tx.tokenId} 
+                      cache={metadataCache}
+                      chainId={chainId}
+                      onNameLoaded={(name) => {
+                        setNftNames(prev => ({ ...prev, [uniqueKey]: name }));
+                      }}
+                    />
+                  </div>
+                  <div className="flex-grow min-w-0">
+                    <div className="flex justify-between items-center">
+                      <div className="text-black dark:text-white"><strong>{tx._type}</strong></div>
+                      <div className="flex items-center">
+                        {tx._type === "Sent" && <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" className="w-5 h-5 ml-2 fill-black dark:fill-white"><path d="M77.17187,170.82861a3.99971,3.99971,0,0,1,0-5.65722L182.34277,60H92a4,4,0,0,1,0-8H192c.0166,0,.03174.0047.04834.00488a4.02377,4.02377,0,0,1,.73437.074c.12159.02411.23389.06927.35108.10412a2.30534,2.30534,0,0,1,.77588.32282c.103.05633.21045.10168.30908.16772a4.02182,4.02182,0,0,1,.58691.47919c.00684.007.01563.01154.02246.01862l.01417.01684a4.0149,4.0149,0,0,1,.48388.5929c.06738.1001.11328.20856.16992.313a3.85529,3.85529,0,0,1,.19776.37573,3.97336,3.97336,0,0,1,.12646.40607c.03321.114.07715.223.10059.34094A3.98826,3.98826,0,0,1,196,56V156a4,4,0,0,1-8,0V65.657L82.82812,170.82861a3.99971,3.99971,0,0,1-5.65625,0ZM216,211.99609H40a4,4,0,0,0,0,8H216a4,4,0,0,0,0-8Z" /></svg>}
+                        {tx._type === "Received" && <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" className="w-5 h-5 ml-2 fill-black dark:fill-white"><path d="M53.17187,114.82471a3.99992,3.99992,0,0,1,5.65625-5.65723L124,174.33936V31.99609a4,4,0,0,1,8,0V174.33936l65.17187-65.17188a3.99992,3.99992,0,1,1,5.65625,5.65723l-72,72c-.00683.00708-.01562.01172-.02246.01855a4.01055,4.01055,0,0,1-.58691.47925c-.10059.06714-.209.11328-.314.17041a3.961,3.961,0,0,1-.37452.197,3.91774,3.91774,0,0,1-.40918.12695c-.11279.03321-.2207.07715-.33789.1001a3.91693,3.91693,0,0,1-1.5664,0c-.11719-.023-.2251-.06689-.33789-.1001a3.91774,3.91774,0,0,1-.40918-.12695,3.961,3.961,0,0,1-.37452-.197c-.105-.05713-.21337-.10327-.314-.17041a4.01055,4.01055,0,0,1-.58691-.47925c-.00684-.00683-.01563-.01147-.02247-.01855ZM216,211.99609H40a4,4,0,0,0,0,8H216a4,4,0,0,0,0-8Z" /></svg>}
+                        {tx._type === "Burned" && <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" className="w-5 h-5 ml-2 fill-black dark:fill-white"><path d="M108,40a4.0002,4.0002,0,0,1,4-4h32a4,4,0,0,1,0,8H112A4.0002,4.0002,0,0,1,108,40Zm36,172H112a4,4,0,0,0,0,8h32a4,4,0,0,0,0-8ZM208,36H184a4,4,0,0,0,0,8h24a4.00458,4.00458,0,0,1,4,4V71.99951a4,4,0,0,0,8,0V48A12.01375,12.01375,0,0,0,208,36Zm8,71.99951a4.0002,4.0002,0,0,0-4,4v32a4,4,0,0,0,8,0v-32A4.0002,4.0002,0,0,0,216,107.99951Zm-176,40a4.0002,4.0002,0,0,0,4-4v-32a4,4,0,1,0-8,0v32A4.0002,4.0002,0,0,0,40,147.99951ZM72,212H48a4.00427,4.00427,0,0,1-4-4V184a4,4,0,0,0-8,0v24a12.01343,12.01343,0,0,0,12,12H72a4,4,0,0,0,0-8ZM50.96,37.30908A4.0003,4.0003,0,0,0,45.04,42.69092l160,175.99951a4,4,0,1,0,5.91992-5.38086Z" /></svg>}
+                        {tx._type === "Minted" && <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" className="w-5 h-5 ml-2 fill-black dark:fill-white"><path d="M208,36H48A12.01312,12.01312,0,0,0,36,48V208a12.01312,12.01312,0,0,0,12,12H208a12.01312,12.01312,0,0,0,12-12V48A12.01312,12.01312,0,0,0,208,36Zm4,172a4.004,4.004,0,0,1-4,4H48a4.004,4.004,0,0,1-4-4V177.65631l33.17187-33.171a4.00208,4.00208,0,0,1,5.65723,0l20.68652,20.68652a12.011,12.011,0,0,0,16.96973,0l44.68652-44.68652a4.00208,4.00208,0,0,1,5.65723,0L212,161.65625Zm0-57.65625L176.48535,114.8291a11.99916,11.99916,0,0,0-16.96973,0L114.8291,159.51562a4.00681,4.00681,0,0,1-5.65723,0L88.48535,138.8291a12.01009,12.01009,0,0,0-16.96973,0L44,166.34393V48a4.004,4.004,0,0,1,4-4H208a4.004,4.004,0,0,1,4,4ZM108.001,92v.00195a8.001,8.001,0,1,1,0-.00195Z" /></svg>}
+                      </div>
+                    </div>
+                    <div className="flex justify-between items-center text-sm mt-1">
+                      <div className="text-black dark:text-white font-medium truncate" title={`${displayName} #${parseInt(tx.tokenId, 16)}`}>
+                        {displayName} #{parseInt(tx.tokenId, 16)}
+                      </div>
+                      <div className="text-black dark:text-white">{formatShortDate(tx.metadata.blockTimestamp)}</div>
                     </div>
                   </div>
-
-                  <div className="flex justify-between items-center text-sm mt-1">
-                    <div className="text-black dark:text-white font-medium truncate" title={`${displayName} #${parseInt(tx.tokenId, 16)}`}>
-                      {displayName} #{parseInt(tx.tokenId, 16)}
-                    </div>
-                    <div className="text-black dark:text-white">{formatShortDate(tx.metadata.blockTimestamp)}</div>
-                  </div>
-
                 </div>
-              </div>
-
                 <div className="flex items-center justify-between text-black dark:text-white mt-1">
                   <a href={`${getExplorerBaseUrl(chainId)}/tx/${tx.hash}`} target="_blank" rel="noopener noreferrer" className="underline">View on Explorer</a>
                   <button onClick={() => setExpandedIndexes(prev => ({ ...prev, [i]: !prev[i] }))} className="text-xs underline">
                     {expandedIndexes[i] ? "Hide" : "Details"}
                   </button>
                 </div>
-
                 {expandedIndexes[i] && (
                   <div className="mt-2 space-y-1 text-xs">
                     <div className="text-black dark:text-white"><strong>From:</strong> <AddressDisplay address={tx.from} chainId={chainId} getExplorerBaseUrl={getExplorerBaseUrl} shortenAddress={shortenAddress} /></div>
@@ -286,8 +278,8 @@ export default function NftTxHistory({ address, chainId }) {
                   </div>
                 )}
               </div>
-            )
-          })}
+            );
+          })
         )}
       </div>
       {txs.length > perPage && (
