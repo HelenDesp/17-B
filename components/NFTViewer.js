8a143676a4ef0f88bf3a8d94425b8f00b53c2728
HelenDesp <helenadespit@gmail.com>
2025-08-13 01:10:50 +0300

    Add files via upload

--- components/NFTViewer.js
+++ components/NFTViewer.js
@@ -96,25 +96,24 @@ export default function NFTViewer({
                 
                 <button
                   onClick={() => handleOpenPalMoji(nft)}
-                  // "bottom-2" and "right-2" have been removed from this className
                   className="absolute z-10 font-mono text-xl text-gray-800 dark:text-white hover:scale-110 transform transition-transform duration-200"
                   aria-label="Create Your PalMoji"
                   style={{
                     fontFamily: '"Doto", monospace',
                     fontWeight: 900,
-                    textShadow: '0.2px 0 #000, -0.2px 0 #000, 0 0.2px #000, 0 -0.2px #000, 0.2px 0.2px #000, -0.2px -0.2px #000, 0.2px -0.2px #000, -0.2px 0.2px #000',
+                    textShadow: '0.1px 0 #000, -0.1px 0 #000, 0 0.1px #000, 0 -0.1px #000, 0.1px 0.1px #000, -0.1px -0.1px #000, 0.1px -0.1px #000, -0.1px 0.1px #000',
                     lineHeight: 0.9,
                     transform: 'scale(1, 1)',
-                    fontSize: '24px',
+                    fontSize: '22px',
                     whiteSpace: 'nowrap',
-                    // New positioning styles are added here
                     bottom: `${iconPositionBottom}px`,
                     right: `${iconPositionRight}px`,
                   }}
                 >
                   <span style={{ position: 'relative', top: `${iconEyeVerticalShift}px` }}>^</span>
                   <span style={{ marginLeft: `${iconEyeSpacing}px`, marginRight: `${iconEyeSpacing}px` }}>w</span>
-                  <span style={{ position: 'relative', top: `${iconEyeVerticaleShift}px` }}>^</span>
+                  {/* Corrected variable name below */}
+                  <span style={{ position: 'relative', top: `${iconEyeVerticalShift}px` }}>^</span>
                 </button>
 				
                 <div className="absolute left-2 bottom-2 z-10">
