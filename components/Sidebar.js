2e05752896277cf13a4b79875a10a51691fddf1d
HelenDesp <helenadespit@gmail.com>
2025-08-13 11:23:58 +0300

    Add files via upload

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -105,7 +105,7 @@ export default function Sidebar({ activeTab, setActiveTab }) {
         </svg>
       ),
       path: "scoreboard",
-    }	
+    },	
     {
       title: "Settings",
       icon: (
