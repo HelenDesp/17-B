6de22e8353e4f9a5b2d0557e7df18ddbb99a9d1b
HelenDesp <helenadespit@gmail.com>
2025-08-17 04:23:18 +0300

    Add files via upload

--- components/Dashboard.js
+++ components/Dashboard.js
@@ -221,9 +221,9 @@ export default function Dashboard() {
 				  onClick={() => open({ view: 'Networks' })}
 				  className="mt-1 text-lg font-medium text-gray-900 dark:text-white flex items-center w-full"
 				>
-				  <span className="w-3 h-3 rounded-full bg-green-400 mr-2"></span>
+				  <span className="w-3 h-3 rounded-full bg-green-500 mr-2"></span>
 				  {formatChainName(chain?.name || "Unknown")}
-				  <svg xmlns="http://www.w3.org/2000/svg" className="ml-2 h-5 w-5 text-green-400" fill="currentColor" viewBox="0 0 20 20">
+				  <svg xmlns="http://www.w3.org/2000/svg" className="ml-2 h-5 w-5 text-green-500" fill="currentColor" viewBox="0 0 20 20">
 					<path d="M10 1L5 8h10l-5-7zm0 18l5-7H5l5 7z" />
 				  </svg>
 				</button>
