18e301c389abbb069547f044599ee5f495f47be4
HelenDesp <helenadespit@gmail.com>
2025-08-23 03:31:39 +0300

    Add files via upload

--- pages/_app.js
+++ pages/_app.js
@@ -54,11 +54,21 @@ function MyApp({ Component, pageProps }) {
   if (!mounted) return null;
 
   return (
-    <ThemeProvider>
-      <ContextProvider cookies={cookieString}>
-        <Component {...pageProps} />
-      </ContextProvider>
-    </ThemeProvider>
+    <>
+      {/* --- ADD THIS ENTIRE HEAD COMPONENT --- */}
+      <Head>
+        <title>Your App Title</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet" />
+      </Head>
+      
+      <ThemeProvider>
+        <ContextProvider cookies={cookieString}>
+          <Component {...pageProps} />
+        </ContextProvider>
+      </ThemeProvider>
+    </>
   );
 }
 
