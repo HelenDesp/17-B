2193eb971083edfa1a75d34558a7c01224255280
HelenDesp <helenadespit@gmail.com>
2025-08-23 11:35:38 +0300

    Add files via upload

--- components/PalMoji.js
+++ components/PalMoji.js
@@ -240,7 +240,7 @@ const generateScreenshotDataURL = async () => {
 
     try {
       // Temporarily force a fixed width to ensure a consistent capture.
-      originalElement.style.width = '459px';
+      originalElement.style.width = '458.7px';
 
       const capturedCanvas = await html2canvas(originalElement, {
         backgroundColor: null,
